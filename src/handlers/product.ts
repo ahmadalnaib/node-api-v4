30256521c6e9f194ef2b6476ea0eacf8d028fdc8
ahmadalanib <alnaib888@gmail.com>
2024-09-08 11:53:20 +0200

    create update product handlers

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -34,3 +34,56 @@ export const getOneProduct= async(req,res)=>{
   res.json({data:product})
 
 }
+
+// create product
+export const createProduct=async(req,res)=>{
+  const product=await prisma.product.create({
+    data: {
+      name: req.body.name as string,
+      belongsToId: req.user.id as string, 
+      description: req.body.description as string,
+      price: req.body.price as number
+
+    }
+  })
+
+  res.json({data:product})
+}
+
+// update product
+
+export const updateProduct=async(req,res)=>{
+  const updated=await prisma.product.update({
+    where:{
+      id_belongsToId:{
+        id:req.user.id,
+        belongsToId:req.user.id
+      },
+    },
+      
+    data:{
+      name:req.body.name
+    }
+  })
+
+  res.json({data:updated})
+}
+
+
+// delete product
+
+export const deleteProduct=async(req,res)=>{
+  const deleted=await prisma.product.delete({
+    where:{
+    id_belongsToId:{
+      id:req.user.id,
+      belongsToId:req.user.id
+    },
+    }
+    
+  })
+
+  res.json({data:deleted})
+}
+
+
