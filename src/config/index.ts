9944beed3d97d22599589d160f142c798c5c22b6
ahmadalanib <alnaib888@gmail.com>
2024-09-15 14:06:59 +0200

    env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,26 @@
 import merge from 'lodash/merge';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const stage= process.env.STAGE || 'local';
+
+let envConfig ;
+
+if (stage === 'local') {
+  envConfig = require('./local').default;
+}
+else if (stage === 'dev') {
+  envConfig = require('./dev').default;
+}
+else if (stage === 'production') {
+  envConfig = require('./prod').default;
+}
+
+export default merge(stage,
+  env:process.env.NODE_ENV,
+  port:process.env.PORT || 3000,
+  secrets:{
+    jwt:process.env.JWT_SECRET,
+    jwtExp:process.env.JWT_EXP,
+  }
+  envConfig);
